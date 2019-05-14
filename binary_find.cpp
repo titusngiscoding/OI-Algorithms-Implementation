e0173690e54d47438bfee91149e54b4983865ab8
Titus Ng <titusngiscoding@gmail.com>
2019-05-14 21:24:03 +0800

    Create binary_find.cpp

---
+++ binary_find.cpp
@@ -0,0 +1,20 @@
+template <typename T>
+int binary_find(T *arr,T *end, T target)
+{
+    int low=0;
+    int high=end-arr;
+    int mid=(high+low)/2;
+    while(low!=high)
+	{
+    	if(target<arr[mid])
+    		high=mid;
+		else if(target>arr[mid])
+			low=mid+1;
+		else
+			return mid;
+			
+		mid=(high+low)/2;
+	}
+    return -1;
+}
+
